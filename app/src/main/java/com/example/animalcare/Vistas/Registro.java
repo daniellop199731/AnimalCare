18f3885ad6c6d629c9e551ee5f6187c39b160d36
SaraOrozco <sara.vanesa@hotmail.com>
2019-08-19 15:59:17 -0500

    Commit: Vistas2

--- app/src/main/java/com/example/animalcare/Vistas/Registro.java
+++ app/src/main/java/com/example/animalcare/Vistas/Registro.java
@@ -2,15 +2,42 @@ package com.example.animalcare.Vistas;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.CalendarView;
+import android.widget.EditText;
+import android.widget.TextView;
+
 
 import com.example.animalcare.R;
 
 public class Registro extends AppCompatActivity {
+    EditText txtNombre;
+    EditText txtCodigo;
+    EditText txtTipo;
+    EditText txtRaza;
+    TextView txtFecha;
+    CalendarView Date;
+    Button btnRegistrar;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_registro);
+        inicializar();
+    }
+
+
+    public void inicializar(){
+        txtNombre = (EditText) findViewById(R.id.txtNombre);
+        txtCodigo = (EditText) findViewById(R.id.txtCodigo);
+        txtTipo = (EditText) findViewById(R.id.txtTipo);
+        txtRaza = (EditText) findViewById(R.id.txtRaza);
+        txtFecha = (TextView) findViewById(R.id.txtFecha);
+        Date = (CalendarView) findViewById(R.id.Date);
+        btnRegistrar = (Button) findViewById(R.id.btnRegistrar);
     }
 }
