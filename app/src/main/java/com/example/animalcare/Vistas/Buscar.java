a0e5038546cbfd6648d32e7270ba468034c4a06e
SaraOrozco <sara.vanesa@hotmail.com>
2019-08-19 14:54:33 -0500

    Commit: Vistas

---
+++ app/src/main/java/com/example/animalcare/Vistas/Buscar.java
@@ -0,0 +1,16 @@
+package com.example.animalcare.Vistas;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.animalcare.R;
+
+public class Buscar extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_buscar);
+    }
+}
